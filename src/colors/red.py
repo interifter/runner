159bc175bcd0c49f3990daa3a334c038a9a8e22a
Interifter <zachary@interift.com>
2020-01-26 14:13:01 -0800

    adding mixers

--- src/colors/red.py
+++ src/colors/red.py
@@ -8,4 +8,4 @@ def write():
 def mix(color=None):
     if not color:
         return " you tried to mix with the wrong color, bro."
-    return f"I don't want to mix with {color.mix()}"
+    return f"I don't want to mix with {color}"
